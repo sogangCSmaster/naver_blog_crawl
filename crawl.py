046e2669c39cad839d7ce6c243e4ac72abf7edf9
sogangCSmaster <cs20131575@u.sogang.ac.kr>
2020-07-01 10:57:51 +0900

    finish

--- crawl.py
+++ crawl.py
@@ -2,6 +2,8 @@ import requests
 import json
 from bs4 import BeautifulSoup
 import sys
+import time
+from selenium import webdriver
 
 #keyword = sys.argv[1]
 #page = sys.argv[2]
@@ -13,6 +15,10 @@ print(keyword, page)
 search_url = "https://search.naver.com/search.naver?date_from=&date_option=0&date_to=&dup_remove=1&nso=&post_blogurl=&post_blogurl_without=&sm=tab_pge&srchby=all&st=sim&where=post&query=" + keyword
 
 hrefList = []
+
+
+# chromedriver path 설정
+driverLocation = './chromedriver/mac/v83/chromedriver'
 
 for p in range(int(page)):
     search = ""
@@ -31,11 +37,19 @@ for p in range(int(page)):
         href = a['href']
         hrefList.append(href)
 
+driver = webdriver.Chrome(driverLocation)
+
 for href in hrefList:
-    res = requests.get(href)
-    html = res.text
-    print(html)
+    res = driver.get(href)
+    html = driver.page_source
+
     soup = BeautifulSoup(html, 'html.parser')
+    src = soup.find('iframe', {'id': 'mainFrame'})
+    src = src['src']
+    post = "http://blog.naver.com" + src
+    blogContent = requests.get(post)
+    blogContent = blogContent.text
+    soup = BeautifulSoup(blogContent, 'html.parser')
     content = soup.find('div', {'class': 'se-main-container'})
     content = content.text
     print(content)
